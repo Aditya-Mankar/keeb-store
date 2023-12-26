cc00b37b51f658c2b20d417de52be23a8140a24c
Aditya Mankar <51234261+Aditya-Mankar@users.noreply.github.com>
2023-12-26 08:44:00 +0530

    Added config server

--- inventory-service/src/main/java/com/keeb/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/keeb/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,28 @@
 package com.keeb.inventoryservice;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class InventoryServiceApplication {
+
+	@Value("${service.version}")
+	private String buildVersion;
+	@Value("${service.message}")
+	private String buildMessage;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		log.info("Build version: " + buildVersion);
+		log.info("Build message: " + buildMessage);
 	}
 
 }
